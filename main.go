d224ae86936402fd9fd1fc1feb45878d67f1978c
lvan100 <lvan100@yeah.net>
2025-09-10 12:06:54 +0800

    Initial commit

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2025 The Go-Spring Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
 
+	"github.com/go-spring/gs-gen/proto"
 	"github.com/spf13/cobra"
 )
 
+// Version of the gs-gen tool
 const Version = "v0.0.1"
 
 func main() {
@@ -40,13 +55,18 @@ func main() {
 			log.Fatalln(err)
 		}
 
+		currDir, err := os.Getwd()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 		for _, e := range entries {
 			if !e.IsDir() {
 				continue
 			}
 			switch e.Name() {
 			case "http":
-				genHttp()
+				proto.GenHttp(currDir)
 			default: // for linter
 			}
 		}
@@ -57,45 +77,4 @@ func main() {
 	if err := root.Execute(); err != nil {
 		os.Exit(-1)
 	}
-}
-
-func genHttp() {
-	currDir, err := os.Getwd()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	dir := filepath.Join(currDir, "idl/http/proto")
-	if err = os.RemoveAll(dir); err != nil {
-		log.Fatalln(err)
-	}
-	if err = os.MkdirAll(dir, os.ModePerm); err != nil {
-		log.Fatalln(err)
-	}
-
-	r, w, err := os.Pipe()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	go func() {
-		f := bufio.NewReader(r)
-		for {
-			line, _, err := f.ReadLine()
-			if err != nil {
-				log.Fatalln(err)
-			}
-			fmt.Print(string(line))
-		}
-		r.Close()
-	}()
-
-	cmd := exec.Command("gs-http-gen", "--server", "--output", dir)
-	cmd.Dir = filepath.Dir(dir)
-	cmd.Stdout = w
-	cmd.Stderr = w
-	if err = cmd.Run(); err != nil {
-		log.Fatalln(err)
-	}
-	w.Close()
 }
